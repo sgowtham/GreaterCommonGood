f46538ccc3a31a809042b0c59e2c786fc8bb1ce4
Gowtham <g@mtu.edu>
2020-10-08 13:09:09 -0400

    vasp2xyz.sh - includes functions.sh and other sanitizations

--- BASH/vasp2xyz.sh
+++ BASH/vasp2xyz.sh
@@ -1,48 +1,53 @@
 #! /bin/bash
- 
-# Script to extract the atomic positions (xyz) from POSCAR and OUTCAR files
-# of VASP and write them to an XYZ file - to be used with Jmol, MolDen and such
-# other third party programs.
- 
-# First written: Gowtham, Tue, 22 Dec 2009 11:11:12 -0500
-# Last modified: Gowtham, Tue, 22 Dec 2009 16:29:36 -0500
- 
-# Essential VASP files
-# If missing, quit the program
-POSCAR="POSCAR"
-OUTCAR="OUTCAR"
-if [[ -e ${POSCAR} || -e ${OUTCAR} ]]
+# 
+# BASH script to extract the atomic positions (xyz) from POSCAR and OUTCAR
+# files of VASP and write them to an XYZ file - to be used with Jmol, MolDen
+# and such other third-party programs. This script needs to be run from the
+# same folder that contains POSCAR and OUTCAR files. The output file will be
+# overwritten, if it exists.
+#
+# Usage:
+# vasp2xyz.sh
+
+# functions.sh
+if [ -e "functions.sh" ]
 then
-  echo
-  echo "  ${POSCAR} and ${OUTCAR} found"
-else
-  echo
-  echo "  ERROR : ${POSCAR} and/or ${OUTCAR} missing"
-  echo
-  exit
+  source functions.sh
 fi
- 
-# Output filename 
-# If missing, quit the program
-OUTPUT="$1"
+
+# Argument check
 if [ $# -ne 1 ]
 then
   echo
-  echo "  ERROR : OUTPUT_FILENAME Missing"
-  echo "  Usage : $(basename $0) OUTPUT_FILENAME"
+  echo "  Usage: $(basename $0) OUTPUT_FILE"
+  echo "   e.g.: $(basename $0) OUTPUT.xyz"
   echo
-  exit
+  exit ${E_ARGS}
 fi
- 
-# Delete scratch files
-rm -f Final.tmp.*
-rm -f ${OUTPUT}.xyz
+
+# Necessary variables
+POSCAR="POSCAR"
+OUTCAR="OUTCAR"
+OUTPUT_FILE="$1"
+OUTPUT_FOLDER=$(dirname $(readlink -f ${OUTPUT_FILE}))
+
+# Validate POSCAR, OUTCAR, OUTPUT_FILE and OUTPUT_FOLDER
+check_existence ${POSCAR}
+check_readable  ${POSCAR}
+check_file      ${POSCAR}
+check_existence ${OUTCAR}
+check_readable  ${OUTCAR}
+check_file      ${OUTCAR}
+check_writable  ${OUTPUT_FOLDER}
+
+# Remove output/temporary files from prior runs, if any
+rm -f Final.tmp.* LINE_NUMBERS.tmp
+rm -f ${OUTPUT_FILE}
  
 # Atoms related information
 # Extracted from POSCAR and OUTCAR
  
 # Total number of atom types
-# Pat Krogel: Tue, 22 Dec 2009 13:13:48 -050
 declare -a ATOM_TYPE=($(sed -n '6 p' ${POSCAR}))
 ATOM_TYPE_TOTAL=${#ATOM_TYPE[@]}
  
@@ -63,17 +68,20 @@ declare -a ATOM_LABEL=($(grep "POTCAR:" ${OUTCAR} | \
                            sed -e 's/_.*//g'))
 ATOM_LABEL_TOTAL=${#ATOM_LABEL[@]}
  
-# Print2Screen
+# Check if POSCAR and OUTCAR are from the same (or successfully completed)
+# simulation
 if [[ "${ATOM_TYPE_TOTAL}" -ne "${ATOM_LABEL_TOTAL}" ]]
 then
   echo
   echo "  ERROR : Atom Type Total (${ATOM_TYPE_TOTAL}) does NOT"
   echo "          match Atom Label Count (${ATOM_LABEL_COUNT})."
   echo "  Please make sure OUTCAR and POSCAR are from the same"
-  echo "  system and simulation."
+  echo "  system and (successfully completed) simulation."
   echo 
   exit
 fi
+
+# Print initial summary
 echo
 echo "  Number of Atom Types   :" ${ATOM_TYPE_TOTAL}
 echo "  Atom Type              :" ${ATOM_LABEL[@]}
@@ -108,14 +116,17 @@ grep -n "POSITION" ${OUTCAR} | \
   sed -e 's/:/ /g' | \
   sed -e 's/POSITION.*//g' > LINE_NUMBERS.tmp
  
+touch ${OUTPUT_FILE}
+truncate -s0 ${OUTPUT_FILE}
+
 FRAME_NUMBER=1
 while read START
 do
   START_HERE=$(expr ${START} + 2)
   END_HERE=$(expr ${START_HERE} + ${ATOMS_TOTAL} - 1)
  
-  echo "${ATOMS_TOTAL}" >> ${OUTPUT}.xyz
-  echo "# Frame Number: ${FRAME_NUMBER}" >> ${OUTPUT}.xyz
+  echo "${ATOMS_TOTAL}" >> ${OUTPUT_FILE}
+  echo "# Frame Number: ${FRAME_NUMBER}" >> ${OUTPUT_FILE}
   sed -n "${START_HERE},${END_HERE} p" ${OUTCAR} | \
     awk '{printf "%12.8f %12.8f %12.8f\n", $1, $2, $3}' > Final.tmp.${FRAME_NUMBER}
  
@@ -123,9 +134,12 @@ do
   while read X Y Z
   do
     echo ${ATOM_LABEL_2[$l]} ${X} ${Y} ${Z} > Final.tmp.${FRAME_NUMBER}.${l}
-    awk '{ printf "%-3s %12.8f %12.8f %12.8f\n", $1, $2, $3, $4 }' Final.tmp.${FRAME_NUMBER}.${l} >> ${OUTPUT}.xyz
+    awk '{ printf "%-3s %12.8f %12.8f %12.8f\n", $1, $2, $3, $4 }' Final.tmp.${FRAME_NUMBER}.${l} >> ${OUTPUT_FILE}
     l=$(expr ${l} + 1)
   done<Final.tmp.${FRAME_NUMBER}
  
   FRAME_NUMBER=$(expr ${FRAME_NUMBER} + 1)
 done
+
+# Remove temporary files from prior runs
+rm -f Final.tmp.* LINE_NUMBERS.tmp
